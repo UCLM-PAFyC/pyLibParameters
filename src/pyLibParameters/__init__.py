01fbf7c2f5f4eda5d05b0cf64a768b8ecc1747bb
DHL <david.hernandez@uclm.es>
2026-07-23 17:17:01 +0200

    Develop refactor, first step

--- src/pyLibParameters/__init__.py
+++ src/pyLibParameters/__init__.py
@@ -5,9 +5,6 @@ from .ui_qt import ParametersManagerDialog
 from .ui_qt import RasterLayerDialog
 from .ui_qt import VectorLayerDialog
 from .ui_qt import VectorLayerFieldDialog
-from .ui_qt import LayerSetDialog
-from .ui_qt import LayerSetDialog
-from .ui_qt import LayerSetDialog
 
 __all__ = [
     "defs_pars",
